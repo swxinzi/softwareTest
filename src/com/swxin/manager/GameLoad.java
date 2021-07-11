9db527b15d89368c12082869a1617984bf4176bd
swxin <2831207404@qq.com>
2021-07-11 11:04:40 +0800

    20210711修改版

--- src/com/swxin/manager/GameLoad.java
+++ src/com/swxin/manager/GameLoad.java
@@ -1,10 +1,8 @@
 package com.swxin.manager;
 
 import javax.swing.*;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.Map;
+import java.io.InputStream;
+import java.util.*;
 
 /**
  * @说明 加载器 用于读取配置文件
@@ -23,7 +21,20 @@ public class GameLoad {
         imageMap.put("up", new ImageIcon("image/tank/play1/player1_up.png"));
         imageMap.put("right", new ImageIcon("image/tank/play1/player1_right.png"));
         imageMap.put("down", new ImageIcon("image/tank/play1/player1_down.png"));
+    }
 
-        Collections
+    //读取地图配置文件
+    private static Properties properties = new Properties();
+    /**
+     * @说明 加载地图配置文件
+     * @param mapID 关卡地图标识 文件ID
+     */
+    public static void mapLoad(int mapID) {
+
+        //得到配置文件路径
+        String mapName = "com/swxin/text/" + mapID + ".map";
+        //通过io流读取文件 得到类加载器
+        ClassLoader classLoader = GameLoad.class.getClassLoader();
+        InputStream is = classLoader.getResourceAsStream(mapName);
     }
 }
